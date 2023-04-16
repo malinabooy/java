c4681c55e3328073edd1578e6df54b4c299fc38d
malinabooy <1anejlbcuh4uk1@gmail.com>
2023-04-16 13:28:12 +0300

    fix and test

--- sem/HW1/formula.java
+++ sem/HW1/formula.java
@@ -9,26 +9,25 @@ public class formula {
         //Работает только для уравнений вида ?2 + 5? = 69, где неизвестно 1 и 4 число.
         System.out.println("Работает только для уравнений вида ?2 + 5? = 69, где неизвестно 1 и 4 число.");
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("Введите вторую цифру: ");
+        System.out.printf("Введите первую цифру: ");
         int numOne = scanner.nextInt();
 
-        System.out.printf("Введите третью цифру: ");
-        int numTwo = scanner.nextInt();
+        System.out.printf("Введите четвертую цифру: ");
+        int numFour = scanner.nextInt();
 
         System.out.printf("Введите чему равно: ");
-        int numThree = scanner.nextInt();
+        int numResult = scanner.nextInt();
         scanner.close();
 
         // задаем уравнение
-        int q, w, e;
-        e = numThree;
-        
-        // перебираем все возможные значения для q и w
-        for (q = 0; q <= 9; q++) {
-            for (w = 0; w <= 9; w++) {
-                // проверяем, является ли e равной q + w, учитывая знаки вопроса
+        int numTwo, numThree;
+  
+        // перебираем все возможные значения для numTwo и numThree
+        for (numTwo = 0; numTwo <= 9; numTwo++) {
+            for (numThree = 0; numThree <= 9; numThree++) {
+                // проверяем, является ли numResult равной numTwo + numThree, учитывая знаки вопроса
                 //У класса String есть статический метод format(): он позволяет задать шаблон объединения строки с данными.
-                String eq = String.format("%d%d + %d%d = %d", numOne, q, w, numTwo, e);
+                String eq = String.format("%d%d + %d%d = %d", numOne, numTwo, numThree, numFour, numResult);
                 if (eval(eq)) {
                     System.out.println("Решение: " + eq);
                     return; // найдено решение, выходим из цикла
